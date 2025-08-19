2f6aec5ad87f76d6be776f04b7fb28c6387d53a5
Cringle4519 <allthingsarepossible99@yahoo.com>
2025-08-19 04:55:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+// server.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Serve static files from "public" folder (create this later)
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("🚀 Unbroken Path Hope is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
